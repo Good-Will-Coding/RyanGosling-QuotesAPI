13d9ae2b4b3737aff1f89be7de0d4e8aa08fe2fa
billy <billyliadphillips@gmail.com>
2019-03-06 14:07:53 +0000

    heroku push

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const quotesLimitGenerator = require("./quotesLimitGenerator");
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.all("*", (req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.get("/api/quotes", (req, res) => {
