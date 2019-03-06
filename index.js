6a35ce80d6b27c90fa0b8455f4e1e0a1d3a52013
billy <billyliadphillips@gmail.com>
2019-03-06 14:09:21 +0000

    heroku push

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.all("*", (req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname + "index.html"));
 });
 
 app.get("/api/quotes", (req, res) => {
